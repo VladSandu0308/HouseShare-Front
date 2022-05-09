1a8548eb1386436abb91d9e9d7ff521d48a7c931
Vlad <vlad.sandu99@yahoo.com>
2022-05-09 22:52:43 +0300

    Date Picker + Number Input

--- src/components/Helped/BookHouse.js
+++ src/components/Helped/BookHouse.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import DatePicker from "react-datepicker";
+import { addDays } from 'date-fns';
 
 const BookHouse = () => {
 
@@ -17,16 +19,16 @@ const BookHouse = () => {
                 <input class="form" id="id" placeholder='Location' onChange={e => setLocation(e.target.value)}/>
               </div>
               <div className='mb-8'>
-                <label className='block text-black text-sm mb-2' for="phone">
+                <label className='block text-black text-sm mb-2' for="start">
                   <strong>Start</strong> of the accomodation period
                 </label>
-                <input class="form" id="start" placeholder='Start Date' onChange={e => setStartDate(e.target.value)}/>
+                <DatePicker className="form" selected={startDate} onChange={(date) => setStartDate(date)} minDate={new Date()} placeholderText="Start Date"/>
               </div>
               <div className='mb-16'>
-                <label className='block text-black text-sm mb-2' for="email">
+                <label className='block text-black text-sm mb-2' for="end">
                   <strong>End</strong> of the accomodation period
                 </label>
-                <input class="form" id="end" type="email" placeholder='End Date' onChange={e => setEndDate(e.target.value)}/>
+                <DatePicker className="form" selected={endDate} onChange={date => setEndDate(date)} minDate={addDays(startDate, 1)} placeholderText="End Date"/>
               </div>
               
               <button className='bg-primary text-white rounded-xl w-28 mx-auto hover:bg-primary/80 transition-colors duration-300'>
--- src/components/Helper/PostHouse.js
+++ src/components/Helper/PostHouse.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import DatePicker from "react-datepicker";
+import { addDays } from 'date-fns';
 
 const PostHouse = () => {
   const [endDate, setEndDate] = useState();
@@ -16,22 +18,22 @@ const PostHouse = () => {
                 <input class="form" id="name" placeholder='Location' onChange={e => setLocation(e.target.value)}/>
               </div>
               <div className='mb-8'>
-                <label className='block text-black text-sm mb-2' for="phone">
+                <label className='block text-black text-sm mb-2' for="start">
                   <strong>Start</strong> of the accomodation period
                 </label>
-                <input class="form" id="phone" placeholder='Start Date' onChange={e => setStartDate(e.target.value)}/>
+                <DatePicker className="form" selected={startDate} onChange={(date) => setStartDate(date)} minDate={new Date()} placeholderText="Start Date"/>
               </div>
               <div className='mb-8'>
                 <label className='block text-black text-sm mb-2' for="email">
                   <strong>End</strong> of the accomodation period
                 </label>
-                <input class="form" id="email" type="email" placeholder='End Date' onChange={e => setEndDate(e.target.value)}/>
+                <DatePicker className="form" selected={endDate} onChange={date => setEndDate(date)} minDate={addDays(startDate, 1)} placeholderText="End Date"/>
               </div>
               <div className='mb-8'>
                 <label className='block text-black text-sm mb-2' for="password">
                   <strong>Sleeping Capacity</strong>
                 </label>
-                <input class="form appearance-none" id="password" type="text" placeholder='Capacity' onChange={e => setCapacity(e.target.value)}/>
+                <input class="form appearance-none" id="password" type="number" min="1" max="16" placeholder='Capacity' onChange={e => setCapacity(e.target.value)}/>
               </div>
               <div className='mb-16'>
                 <label className='block text-black text-sm mb-2' for="phone">
